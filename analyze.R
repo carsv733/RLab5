406e0e391dbaaae5f15b130e074a5faeafbb9bfe
carsv733 <carsv733@student.liu.se>
2015-09-29 09:07:35 +0200

    Function! Yai!

--- analyze.R
+++ analyze.R
@@ -6,16 +6,47 @@ library(httr)
 install.packages("jsonlite")
 library(jsonlite)
 
-val_get<-function() {
-  temp <- "http://api.kolada.se/v1/data/permunicipality/N00092/0580"
-  to30 <- fromJSON(txt=temp)
-  temp <- "http://api.kolada.se/v1/data/permunicipality/N00093/0580"
-  to49 <- fromJSON(txt=temp)
-  temp <- "http://api.kolada.se/v1/data/permunicipality/N00094/0580"
-  up49 <- fromJSON(txt=temp)
-  total<-merge(to30$values[,3:4],to49$values[,3:4],by="period")
-  total<-merge(total,up49$values[,3:4],by="period")
-  total
+
+myData <- electionData(c("0136", "0140"), "D:/Dokument/732A50/")
+
+electionData <- function(kommun, path) {
+
+#För möjliga argument "kommun" och "path" (path=sökväg)
+list <- list.files(paste(path, "slutresultat/", sep=""),full.names=T,pattern = "*R.xml")
+
+folder <- "slutresultat/slutresultat_"
+format <- "R.xml"
+
+file<-c()
+for (i in seq(length(kommun))) {
+  file[i] <- as.character(paste(path, folder, kommun[i], format, sep=""))
 }
 
-test<-val_get()
+#Nu går detta köra med en 
+
+listTest<-c()
+for (i in seq(length(list))) {
+  if (list[i]%in%file) {
+    listTest <- append(listTest, list[i])
+  }
+}
+
+datafr <- data.frame()
+for(i in seq(length(file))) {
+  dataTest <- xmlTreeParse(listTest[i])
+  r <- xmlRoot(dataTest)
+  k <- xmlToList(dataTest)
+  dataTest <- k$KOMMUN$GILTIGA
+  temps <- r[["KOMMUN"]]
+  namn <- xmlAttrs(temps)[["NAMN"]]
+  temps <- temps[names(temps) == "GILTIGA"]
+  df <- data.frame(matrix(unlist(temps), ncol=7, byrow=T),stringsAsFactors=FALSE)
+  df$X1 <- NULL
+  colnames(df) <- c("Parti", "Röster","Röster_FGVal","Procent","Procent_FGVal","Procent_Ändring")
+  df["Kommun"] <- NA
+  df$Kommun <- namn
+  datafr <- rbind(datafr,df)
+}
+return(datafr)
+}
+
