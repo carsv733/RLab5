4bca0f256f1b07e1ad97555068e8de76407f96e4
carsv733 <carsv733@student.liu.se>
2015-09-30 10:39:36 +0200

    Fixed checkbox problems!

--- App-1/server.R
+++ App-1/server.R
@@ -1,40 +1,36 @@
 
 library(shiny)
 
-counts <- tapply(myData$Percent, list(as.factor(myData$Party), as.factor(myData$Kommun)), mean)
-
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-  
-  # Expression that generates a histogram. The expression is
-  # wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should re-execute automatically
-  #     when inputs change
-  #  2) Its output type is a plot
+  counts <- tapply(myData$Percent, list(as.factor(myData$Party), as.factor(myData$Kommun)), mean)
+  colours <- c("darkblue", "green", "blue", "orange", "red" ,"darkred", "seagreen", "lightblue", "pink")
 
   
-  output$plot <- renderPlot({
+ distplot <- eventReactive(input$action, {
+   if (10 %in% input$select) { 
+     
+     barplot(counts, main="Votes, by municipal and party",
+             xlab="Municipal", col=colours,
+             legend = myData$Party[1:9] , beside=TRUE)
+     
+   } else {
+     
+     partyIndex <- 1:9
+     toDelete <- partyIndex[-as.numeric(input$select)]
+     
+     counts <- counts[-toDelete,]
+     
+     
+     barplot(counts, main="Votes, by municipal and party",
+             xlab="Municipal", col=colours[seq(length(input))],
+             legend = rownames(counts) , beside=TRUE)
+   }
+  })
+  
+  output$distPlot <- renderPlot({
     
-    
-    if (10 %in% input$select) {   
-      
-    barplot(counts, main="Votes, by municipal and party",
-            xlab="Municipal", col=c("darkblue", "green", "blue", "orange", "red" ,"darkred", "seagreen", "lightblue", "pink"),
-            legend = myData$Party[1:9] , beside=TRUE)
-    
-    } else {
-            
-       for (i in 1:9) {
-         #verkar inte finnas i inputselect!
-        if (i%in%input$select==FALSE) {
-          counts[-i,]
-        }
-      }
-      barplot(counts, main="Votes, by municipal and party",
-              xlab="Municipal", col=c("lightblue"),
-              legend = myData$Party[1:9] , beside=TRUE)
-    }
+  distplot()
     
   })
 })
